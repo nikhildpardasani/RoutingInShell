b6caccb50353c92a2a42773c44363c11426b6deb
Nikhil Pardasani <nikhil_pardasani@yahoo.com>
2019-12-23 14:54:51 +0530

    Added subordinate pages

---
+++ RoutingInShell/Views/Tab1A.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RoutingInShell.Views
+{
+    public partial class Tab1A : ContentPage
+    {
+        public Tab1A()
+        {
+            InitializeComponent();
+        }
+    }
+}
